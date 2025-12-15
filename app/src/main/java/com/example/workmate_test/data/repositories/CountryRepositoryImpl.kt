6b3dc76d06fd1a80ef8d6aa1f8343f549b3a5a8a
Gnarll <vadimfedorovich22@mail.ru>
2025-12-15 10:53:11 +0300

    fix refreshing data logic

--- app/src/main/java/com/example/workmate_test/data/repositories/CountryRepositoryImpl.kt
+++ app/src/main/java/com/example/workmate_test/data/repositories/CountryRepositoryImpl.kt
@@ -7,8 +7,13 @@ import com.example.workmate_test.data.mappers.toCountryEntity
 import com.example.workmate_test.domain.models.Country
 import com.example.workmate_test.domain.repositiories.CountryRepository
 import com.example.workmate_test.domain.utils.Result
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.transformLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.serialization.SerializationException
 import retrofit2.HttpException
 import java.io.IOException
@@ -18,35 +23,43 @@ class CountryRepositoryImpl @Inject constructor(
     private val countryDao: CountryDao,
     private val countryApi: CountryApi
 ) : CountryRepository {
+    private val refreshTrigger = MutableStateFlow(0)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun getCountriesSteam(): Flow<Result<List<Country>>> {
-        return countryDao.getCountries().transform { entities ->
-            emit(Result.Loading)
+        return refreshTrigger.flatMapLatest {
+            
+            countryDao.getCountries().transformLatest { entities ->
+                emit(Result.Loading)
 
-            if (entities.isEmpty()) {
-                try {
-                    val countryEntities = countryApi.getCountries()
-                        .map { it.toCountryEntity() }
-                    countryDao.insertCountries(countryEntities)
-                } catch (e: IOException) {
-                    emit(Result.Error.NetworkError(e))
-                } catch (e: HttpException) {
-                    emit(Result.Error.ServerError(e))
-                } catch (e: SerializationException) {
-                    emit(Result.Error.SerializationError(e))
-                } catch (e: Throwable) {
-                    emit(Result.Error.UnknownError(e))
+                if (entities.isEmpty()) {
+                    try {
+                        val countryEntities = countryApi.getCountries()
+                            .map { it.toCountryEntity() }
+                        countryDao.insertCountries(countryEntities)
+                    } catch (e: IOException) {
+                        emit(Result.Error.NetworkError(e))
+                    } catch (e: HttpException) {
+                        emit(Result.Error.ServerError(e))
+                    } catch (e: SerializationException) {
+                        emit(Result.Error.SerializationError(e))
+                    } catch (e: Throwable) {
+                        emit(Result.Error.UnknownError(e))
+                    }
                 }
-            } else {
-                val countries = entities.map { it.toCountry() }
-                emit(Result.Success(countries))
+
+                if (entities.isNotEmpty()) {
+                    val countries = entities.map { it.toCountry() }
+                    emit(Result.Success(countries))
+                }
             }
-
         }
-
+            .distinctUntilChanged()
     }
 
     override suspend fun refreshCountries() {
         countryDao.deleteAllCountries()
+        refreshTrigger.update { it + 1 }
     }
 
     override suspend fun getCountry(id: Int): Result<Country> {
